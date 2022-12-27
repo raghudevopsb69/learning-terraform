bb7676e42b3e8a32a736296f84d56d457e651ccd
Raghu K <raghuk.vit@gmail.com>
2022-12-27 08:58:58 +0530

    Update learning-terraform/

--- 02-variables/variables.tf
+++ 02-variables/variables.tf
@@ -2,7 +2,53 @@ variable "sample" {
   default = "Hello"
 }
 
+output "samplex" {
+  value = var.sample
+}
+
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+
+variable "list1" {
+  default = [
+    "hello",
+    100,
+    true
+  ]
+}
+
+variable "map1" {
+  default = {
+    string  = "Helo"
+    number  = 100
+    boolean = true
+  }
+}
+
+// Refer normal variable
 output "sample" {
   value = var.sample
+}
+
+// Refer list variable, print 0 index value
+output "sample1" {
+  value = var.sample1[0]
+}
+
+// Refer map variable, print string key value
+output "sample2" {
+  value = var.sample2["string"]
 }
 
