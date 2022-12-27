e3cdf9684f66aad238899838e8ce6aa96ca98240
Raghu K <raghuk.vit@gmail.com>
2022-12-27 08:51:00 +0530

    Update learning-terraform/

--- 01-outputs/output.tf
+++ 01-outputs/output.tf
@@ -3,9 +3,9 @@ output "test" {
 }
 
 data "aws_ami" "example" {
-  executable_users = ["self"]
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
 }
 
 output "AMI_ID" {
