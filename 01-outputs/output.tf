28193fed4757efb55c9eead73a89902aefdf4fb7
Raghu K <raghuk.vit@gmail.com>
2022-12-27 08:49:56 +0530

    Update learning-terraform/

--- 01-outputs/output.tf
+++ 01-outputs/output.tf
@@ -2,3 +2,13 @@ output "test" {
   value = "Helo"
 }
 
+data "aws_ami" "example" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
+
+output "AMI_ID" {
+  value = data.aws_ami.example.id
+}
+
