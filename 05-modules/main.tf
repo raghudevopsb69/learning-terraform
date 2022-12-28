95136b481d1965305412f1f4e8f7d2dc343a70f7
Raghu K <raghuk.vit@gmail.com>
2022-12-28 08:34:53 +0530

    Update learning-terraform/

--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -5,5 +5,12 @@ module "sg" {
 module "ec2" {
   source = "./ec2"
   sgid   = module.sg.sgid
+  ami    = "ami-0a017d8ceb274537d"
+}
+
+module "ec21" {
+  source = "./ec2"
+  sgid   = module.sg.sgid
+  ami    = "ami-0b5eea76982371e91"
 }
 
