7ca8fe48733931cc79915a69c0051136b086d9b6
Raghu K <raghuk.vit@gmail.com>
2022-12-28 08:42:24 +0530

    Update learning-terraform/

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "test" {
   count         = 2
   ami           = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
+  tags = {
+    Name = "instance-${count.index}"
+  }
 }
 
 output "public_ip" {
