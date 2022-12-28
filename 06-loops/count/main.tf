60dcecfeee9a684f6be6f52a6bf61fa419638fb4
Raghu K <raghuk.vit@gmail.com>
2022-12-28 08:46:09 +0530

    Update learning-terraform/

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -1,12 +1,30 @@
-resource "aws_instance" "test" {
-  count         = 2
-  ami           = "ami-0a017d8ceb274537d"
+//resource "aws_instance" "test" {
+//  count         = 2
+//  ami           = "ami-0a017d8ceb274537d"
+//  instance_type = "t3.micro"
+//  tags = {
+//    Name = "instance-${count.index}"
+//  }
+//}
+//
+//output "public_ip" {
+//  value = aws_instance.test.*.public_ip
+//}
+
+variable "amis" {
+  default = [
+    "ami-0a017d8ceb274537d",
+    "ami-0b5eea76982371e91"
+  ]
+}
+
+resource "aws_instance" "test1" {
+  count = length(var.amis)
+  ami   = var.amis[count.index]
+  //ami           = element(var.amis, count.index)
   instance_type = "t3.micro"
   tags = {
-    Name = "instance-${count.index}"
+    Name = "ami-${count.index}"
   }
 }
 
-output "public_ip" {
-  value = aws_instance.test.*.public_ip
-}
