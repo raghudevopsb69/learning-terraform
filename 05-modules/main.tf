db054cf0b5a3623852820e63f68e1a98e57087dd
Raghu K <raghuk.vit@gmail.com>
2022-12-28 08:32:29 +0530

    Update learning-terraform/

--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -2,3 +2,8 @@ module "sg" {
   source = "./sg"
 }
 
+module "ec2" {
+  source = "./ec2"
+  sgid   = module.sg.sgid
+}
+
--- 05-modules/sg/sg.tf
+++ 05-modules/sg/sg.tf
@@ -24,4 +24,7 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
+output "sgid" {
+  value = aws_security_group.allow_tls.id
+}
 
