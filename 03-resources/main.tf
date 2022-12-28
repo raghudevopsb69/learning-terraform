01407e2daebf91e1650edf04680686987b6ad6fa
Raghu K <raghuk.vit@gmail.com>
2022-12-28 08:11:59 +0530

    Update learning-terraform/

--- 03-resources/main.tf
+++ 03-resources/main.tf
@@ -1,8 +1,36 @@
 resource "aws_instance" "test" {
-  ami           = "ami-0a017d8ceb274537d"
-  instance_type = "t3.micro"
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
 
 output "public_ip" {
   value = aws_instance.test.public_ip
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-042992ad68653bd97"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
